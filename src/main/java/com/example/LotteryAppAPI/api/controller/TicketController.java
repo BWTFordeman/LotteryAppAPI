e06085a8e52c9607802c117e556cb7a1c8f16670
BWTFordeman <cbt_97@hotmail.com>
2023-11-27 20:05:52 +0100

    Setup working requests

--- src/main/java/com/example/LotteryAppAPI/api/controller/TicketController.java
+++ src/main/java/com/example/LotteryAppAPI/api/controller/TicketController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://zealous-forest-0040a6803.4.azurestaticapps.net/")
 @RestController
 @RequestMapping("/")
 public class TicketController {
